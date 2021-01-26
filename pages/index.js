398b3d601a11ba25b329907377b3ab4056ecee9f
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-01-25 23:01:08 -0300

    add: add metatags

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import db from '../db.json';
 
+import Head from 'next/head';
+
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
@@ -9,31 +11,50 @@ import GithubCorner from '../src/components/GithubCorner';
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
+    <>
+      <Head>
+        <title>Aluraquiz - SadS4ndWiCh</title>
 
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+        <meta name='title' content='Aluraquiz - SadS4ndWiCh' />
+        <meta name='description' content='Projeto Aluraquiz da Imersão React da Alura criado em NextJS' />
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://aluraquiz.sads4ndwich.vercel.app/' />
+        <meta property='og:title' content='Aluraquiz - SadS4ndWiCh' />
+        <meta property='og:description' content='Projeto Aluraquiz da Imersão React da Alura criado em NextJS' />
 
-            <p>Lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://aluraquiz.sads4ndwich.vercel.app/' />
+        <meta property='twitter:title' content='Aluraquiz - SadS4ndWiCh' />
+        <meta property='twitter:description' content='Projeto Aluraquiz da Imersão React da Alura criado em NextJS' />
+      </Head>
 
-        <Footer />
-      </QuizContainer>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
 
-      <GithubCorner projectUrl='https://github.com/SadS4ndWiCh/aluraquiz' />
-    </QuizBackground>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
+
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
+
+              <p>Lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+
+          <Footer />
+        </QuizContainer>
+
+        <GithubCorner projectUrl='https://github.com/SadS4ndWiCh/aluraquiz' />
+      </QuizBackground>
+    </>
   )
 }
