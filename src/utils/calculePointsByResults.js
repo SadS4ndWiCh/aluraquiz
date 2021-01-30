31ac33b9a01fc6b862def2b6f6f5fda5ce8f2f3f
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-01-29 22:20:26 -0300

    update: now can negative points

--- src/utils/calculePointsByResults.js
+++ src/utils/calculePointsByResults.js
@@ -2,6 +2,6 @@ export default function calculePointsByResults(results) {
   return results.reduce((totalPoints, currentIsCorrect) => {
     currentIsCorrect ? totalPoints += 100 : totalPoints -= 25;
 
-    return Math.max(0, totalPoints);
+    return totalPoints;
   }, 0);
 }
